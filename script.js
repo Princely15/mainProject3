c1b869153593e64c0cee57bb30becbf6c0f76e09
Princely15 <richmondoginni@rocketmail.com>
2026-06-07 22:35:53 -0500

    Updated Function of generate meme and each generated meme now has a delete and text

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ const topText = document.getElementById("top-text");
 const bottomText = document.getElementById("bottom-text");
 
 const generateBtn = document.getElementById("generate-btn");
-const topValue = topInput.value.trim();
-const bottomValue = bottomInput.value.trim();
 const memeHistory = document.getElementById("meme-history");
 const clearBtn = document.getElementById("clear-btn");
 
@@ -33,11 +31,12 @@ generateBtn.addEventListener("click", () => {
   topText.textContent = topValue;
   bottomText.textContent = bottomValue;
 
-  memes.push({
-    image: imageUrl,
-    top: topValue,
-    bottom: bottomValue
-  });
+memes.push({
+  id: Date.now(),
+  image: imageUrl,
+  top: topValue,
+  bottom: bottomValue
+});
 
   if (memes.length > 5) {
     memes.shift();
@@ -46,15 +45,55 @@ generateBtn.addEventListener("click", () => {
   displayMemes();
 });
 
+function deleteMeme(id) {
+  memes = memes.filter(meme => String(meme.id) !== String(id));
+  displayMemes();
+}
+
 function displayMemes() {
   memeHistory.innerHTML = "";
 
   memes.forEach((meme) => {
-    memeHistory.innerHTML += `<img src="${meme.image}" width="100">`;
+    const memeDiv = document.createElement("div");
+   memeDiv.classList.add("meme", "history-meme");
+
+    const img = document.createElement("img");
+    img.src = meme.image;
+
+    const topCaption = document.createElement("div");
+    topCaption.classList.add("text", "top-text");
+    topCaption.textContent = meme.top;
+
+    const bottomCaption = document.createElement("div");
+    bottomCaption.classList.add("text", "bottom-text");
+    bottomCaption.textContent = meme.bottom;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+   
+    deleteBtn.dataset.id = meme.id;
+
+    deleteBtn.addEventListener("click", (e) => {
+      deleteMeme(e.target.dataset.id);
+    });
+
+    memeDiv.append(img, topCaption, bottomCaption, deleteBtn);
+    memeHistory.appendChild(memeDiv);
   });
 }
+
+
+
+
+
+
 
 clearBtn.addEventListener("click", () => {
   memes = [];
   displayMemes();
+
+  memeImage.src = "";
+  topText.textContent = "";
+  bottomText.textContent = "";
 });
